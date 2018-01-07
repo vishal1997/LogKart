5e80bc4f615071bb11c87ef886c822b4065e0159
Harsh8chawda <harshchawda8@gmail.com>
2018-01-07 20:00:44 +0530

    [Some issues fixed]

--- src/app/routing/app-router.module.ts
+++ src/app/routing/app-router.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../body/main-content/home/home.component'
 import { LoginComponent} from '../body/login/login.component'
 import {RegisterComponent} from '../body/register/register.component'
+import {SettingComponent} from '../body/setting/setting.component'
+import {ContactUsComponent} from '../body/contact-us/contact-us.component'
+import { ForgotPasswordComponent } from '../body/forgot-password/forgot-password.component'
 
 const routes:Routes = [
     {path:"home", component: HomeComponent },
     {path:"login", component: LoginComponent},
-    {path:"register", component: RegisterComponent}
+    {path:"register", component: RegisterComponent},
+    {path:"setting",component:SettingComponent},
+    {path:"contactus",component:ContactUsComponent},
+    {path:"forgotpassword",component:ForgotPasswordComponent}
 ]
 
 @NgModule({
